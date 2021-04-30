d7e09259ec182bdcea198c674cf05c5bc0a935f3
Mridul20 <mridulmittal20@gmail.com>
2021-04-30 16:18:26 +0530

    check

--- app/src/main/java/com/example/onlinevotingsystem/constants/TableKeys.java
+++ app/src/main/java/com/example/onlinevotingsystem/constants/TableKeys.java
@@ -53,7 +53,9 @@ public class TableKeys {
     public static final String KEY_POLL_NO_VOTERS="no_voters";
     public static final String KEY_POLL_ELEC_START_TIME="elec_start_time";
     public static final String KEY_POLL_ELEC_END_TIME="elec_end_time";
-    public static final String KEY_POLL_NO_VOTES_CASTED="no_votes_casted";    
+    public static final String KEY_POLL_NO_VOTES_CASTED="no_votes_casted";
 
+
+    public static final String GIT_CHECK="check";
 
 }
